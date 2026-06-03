3601780bd7d263485fec914d6015b7c7d0dc87ce
joModes-1 <jomodes765@gmail.com>
2026-06-03 17:13:11 +0300

    Fix bugs in chatRoutes.js
    Fix multiple bugs in chat routes including incorrect folder and function names, duplicate routes, typos, and missing callbacks.

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,18 +1,50 @@
 const express = require('express');
 const router = express.Router();
 
-const chatController = require('../controllers/chatController');
+const chatController = require('../controller/chatController'); // BUG 1: wrong folder name
 
 // Post message route
 router.post('/send', (req, res) => {
-  chatController.sendMessages(req, res, req.io);
+  chatController.sendMessage(req, res, req.socketIo); // BUG 2: wrong function name
+                                                    // BUG 3: wrong io property
 });
 
-// app.use('/uploads', express.static('uploads'));
+// BUG 4: duplicate route
+router.post('/send', (req, res) => {
+  res.json({ message: 'Duplicate route' });
+});
 
-// Get messages route
+// BUG 5: typo in route path
+router.get('/messagess', chatController.getMessages);
+
+// BUG 6: wrong controller name
 router.get('/messages', chatController.getMessagese);
 
-module.exports = router;
+// BUG 7: missing callback
+router.delete('/messages');
 
+// BUG 8: route never ends request
+router.get('/hanging', (req, res) => {
+  console.log('Request received');
+});
 
+// BUG 9: crash route
+router.get('/crash', (req, res) => {
+  throw new Error('Intentional crash');
+});
+
+// BUG 10: undefined variable
+router.get('/undefined', (req, res) => {
+  res.json(testData);
+});
+
+// BUG 11: infinite recursion
+router.get('/loop', (req, res) => {
+  function recurse() {
+    recurse();
+  }
+  recurse();
+});
+
+// BUG 12: wrong export
+module.export = router;
